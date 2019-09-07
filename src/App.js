a1b53f60134fa2d8797c5b28843b7a481fbba72a
rffffy <raafay.alam@gmail.com>
2019-09-07 02:05:10 +0200

    routing for react added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => <h1>Hats Page</h1>;
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<HomePage></HomePage>
+				<Route exact path='/' component={HomePage} />
+				<Route exact path='/shop/hats' component={HatsPage} />
 			</div>
 		);
 	}
