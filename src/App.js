6663558e9c299ca5c02ce8088d450c840fc35b64
rffffy <raafay.alam@gmail.com>
2019-09-07 14:51:25 +0200

    updated with shop page and collections

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
-const HatsPage = () => <h1>Hats Page</h1>;
+// const HatsPage = () => <h1>Hats Page</h1>;
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<Route exact path='/' component={HomePage} />
-				<Route exact path='/shop/hats' component={HatsPage} />
+				<Route exact path='/shop' component={ShopPage} />
 			</div>
 		);
 	}
