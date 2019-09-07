c834006d5e20d6d01bf85926d08694b14752e60c
rffffy <raafay.alam@gmail.com>
2019-09-07 22:06:31 +0200

    sign in page addded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component.jsx";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.componenet";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.componenet";
 
 // const HatsPage = () => <h1>Hats Page</h1>;
 
@@ -12,8 +14,12 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<Route exact path='/' component={HomePage} />
-				<Route exact path='/shop' component={ShopPage} />
+				<Header />
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route path='/shop' component={ShopPage} />
+					<Route path='/signin' component={SignInAndSignUp} />
+				</Switch>
 			</div>
 		);
 	}
