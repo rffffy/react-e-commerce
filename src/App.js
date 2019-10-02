d6bff3a704c2e0ccf9847def31ef04574b7efd75
rffffy <raafay.alam@gmail.com>
2019-10-03 01:10:43 +0200

    sagas added for sign out and cart dropdown clear

--- src/App.js
+++ src/App.js
@@ -14,23 +14,14 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from "./pages/checkout/checkout.component";
 
 import { selectCurrentUser } from "./redux/user/user.selectors";
+import { checkUserSession } from "./redux/user/user.actions";
 
 class App extends Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		// this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-		// 	if (userAuth) {
-		// 		const userRef = await createUserProfileDocument(userAuth);
-		// 		userRef.onSnapshot(snapshot => {
-		// 			setCurrentUser({
-		// 				id: snapshot.id,
-		// 				...snapshot.data()
-		// 			});
-		// 		});
-		// 	}
-		// 	setCurrentUser(userAuth);
-		// });
+		const { checkUserSession } = this.props;
+		checkUserSession();
 	}
 
 	componentWillUnmount() {
@@ -66,4 +57,11 @@ const mapStateToProps = createStructuredSelector({
 	currentUser: selectCurrentUser
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => ({
+	checkUserSession: () => dispatch(checkUserSession())
+});
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(App);
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,24 +1,20 @@
 import { takeLatest, put, all, call } from "redux-saga/effects";
 
-import { signInSuccess, signInFailure } from "./user.actions";
+import {
+	signInSuccess,
+	signInFailure,
+	signOutSuccess,
+	signOutFailure
+} from "./user.actions";
 
 import {
 	auth,
 	googleProvider,
-	createUserProfileDocument
+	createUserProfileDocument,
+	getCurrentUser
 } from "../../firebase/firebase.utils";
 
 import UserActionTypes from "./user.types";
-
-export function* getSnapshotFromUserAuth(userAuth) {
-	try {
-		const userRef = yield call(createUserProfileDocument, userAuth);
-		const userSnapshot = yield userRef.get();
-		yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
-	} catch (error) {
-		yield put(signInFailure(error));
-	}
-}
 
 export function* signInWithGoogle() {
 	try {
@@ -38,6 +34,35 @@ export function* signInWithEmail({ payload: { email, password } }) {
 	}
 }
 
+export function* isUserAuthenticated() {
+	try {
+		const userAuth = yield getCurrentUser();
+		if (!userAuth) return;
+		yield getSnapshotFromUserAuth(userAuth);
+	} catch (error) {
+		yield put(signInFailure(error));
+	}
+}
+
+export function* getSnapshotFromUserAuth(userAuth) {
+	try {
+		const userRef = yield call(createUserProfileDocument, userAuth);
+		const userSnapshot = yield userRef.get();
+		yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+	} catch (error) {
+		yield put(signInFailure(error));
+	}
+}
+
+export function* signOut() {
+	try {
+		yield auth.signOut();
+		yield put(signOutSuccess());
+	} catch (error) {
+		yield put(signOutFailure(error));
+	}
+}
+
 export function* onGoogleSignInStart() {
 	yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -46,6 +71,19 @@ export function* onEmailSignInStart() {
 	yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
 }
 
+export function* onCheckUserSession() {
+	yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
+export function* onSignOutStart() {
+	yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
+}
+
 export function* userSagas() {
-	yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
+	yield all([
+		call(onGoogleSignInStart),
+		call(onEmailSignInStart),
+		call(isUserAuthenticated),
+		call(onSignOutStart)
+	]);
 }
